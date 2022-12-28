6c83b167585c243a191828e1270cbc3e72043b49
Neal Harrington <ngharrington@users.noreply.github.com>
2022-12-27 20:09:02 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import "./App.css";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div styles={{ marginLeft: "0px" }}>
+        <h1>make a littly bitty baby url</h1>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Paper
+              component="form"
+              sx={{ p: "2px 4px", display: "flex", alignItems: "center" }}
+            >
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Make Tiny Urls"
+                inputProps={{ "aria-label": "make tiny urls" }}
+              />
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Button variant="contained">Shorten Url</Button>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
